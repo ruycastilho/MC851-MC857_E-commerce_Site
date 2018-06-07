75d3464e7289be8102652615c93678bfcc490123
akira <victor.akira.hs@gmail.com>
2018-06-07 01:25:32 -0300

    added products api

--- backend/mc85x_backend/urls.py
+++ backend/mc85x_backend/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('sac.urls')),
     path('api/', include('cart.urls')),
+    path('api/', include('products.urls')),
 ]
