caccdaf727fd00deeba37574a24695876bbf3e44
rRuy Castilho Barrichelo <ruy.castilho26@gmail.com>
2018-06-10 13:08:15 -0300

    Changed main page to display product details

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,35 +1,36 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
-import { Container, Row, Col } from 'reactstrap';
+import { Form, FormGroup, Input, Media, Collapse, Button, CardBody, Card, Container, Row, Col } from 'reactstrap';
 import producttest from '../producttest.jpg';
+import "../Products.css";
+
 
 const MainDiv = styled.div`
-width: 100%;
-margin: 2em auto;
+    width: 100%;
+    margin: 2em auto;
 `;
 
 
-const ProdImg = styled.div`
-width: 100%;
-padding-top: 133%;
-margin: 0.25em 0;
-background: white url(${props => props.src}) no-repeat right top;
-background-size: contain;
-text-align: center;
+const ProdImgDiv = styled.div`
+    max-height: 100px;
+    padding: 10px;
+    width: auto;
+    padding-top: 133%;
+    background: white url(${props => props.src}) no-repeat top;
+    background-size: contain;
 `;
 
 const Legend = styled.p `
-font-size: 20px;
-color: black;
-position: absolute;
-bottom: 20%;
-margin: 0 auto;
-width: calc(100% - 30px); 	// por causa do padding do avô
+    font-size: 20px;
+    color: black;
+    margin: 0 auto;
+    width: calc(100% - 30px); 	// por causa do padding do avô
+    margin-top: 15px;
+    
 `;
 
 const SubLeg = Legend.extend `
-font-size: 15px;
-bottom: 10%;
+    font-size: 15px;
 `;
 
 // aqui a gente coloca as imagens de cada produto
@@ -62,38 +63,95 @@ const subLegends =[
     "89x de R$666,66",  
 ];
 
-function retCols(){
-    var ret = [];
-    var number;
-    if(imgSrcs.length == legends.length && legends.length === subLegends.length)
-	number = subLegends.length;
-    else
-	number = 0;
-    for(var i=0; i<number; i++)
-	ret.push(
-	    <Col xl="2" sm="4" xs="6">
-	      <ProdImg src={imgSrcs[i]}>
-		<Legend>{legends[i]}</Legend>
-		<SubLeg>{subLegends[i]}</SubLeg>
-	      </ProdImg>
-	    </Col>
-	);
-    return ret;
-}
-
-const cols = retCols(6);
 
 
-class Products extends Component {
+class Product extends Component {
+    constructor(props) {
+        super(props);
+        this.toggle = this.toggle.bind(this);
+        this.state = { collapse: false };
+    }
 
+    toggle() {
+        this.setState({ collapse: !this.state.collapse });
+    }
 
     render() {
+        const cart = this.props.amount != 0 ? (
+            <Form class=" text-center">
+                <FormGroup >
+                    <div class="text-center">
+                        <Input type="number" name="number" id="exampleNumber" placeholder="Quantidade" />
+                    </div>
+                </FormGroup>
+                    <div class="button-cart text-center">
+                        <Button color="danger">Adicionar</Button>
+                    </div>
+            </Form>
+        ) : (
+            <div class="text-center">
+                <SubLeg>Produto Indisponível</SubLeg>
+            </div>
+        );
+
+        return (
+            <Col className="col-12 col-sm-4 col-lg-3">
+                <img className="col-6 offset-3 offset-md-0 col-md-12" src={this.props.src} />
+                <Col className="col-12">
+                    <div class="text-center">
+                        <Media body>
+                            <Media heading>
+                                <Legend>{this.props.legend}</Legend>
+                            </Media>
+                            <SubLeg>{this.props.sublegend}</SubLeg>
+                        </Media>
+                    </div>
+                </Col>
+                <Col className="col-12">
+                    <div class="text-center">
+                        <Button color="link" onClick={this.toggle} style={{ marginBottom: '1rem' }}>Detalhes</Button>
+                    </div>
+                    <Collapse isOpen={this.state.collapse}>
+                        <Card>
+                            <CardBody >
+                                <div class="text-center">
+                                    <Legend>Estoque:{this.props.amount} </Legend>
+                                </div>
+                                {cart}
+                            </CardBody>
+                        </Card>
+                    </Collapse>
+                </Col>
+            </Col>
+
+        )
+    }
+}
+  
+
+class Products extends Component {
+    render() {
+    const products = [
+        {legend: "Iphone X 16GB", sublegend:"8x de R$666,66", src:producttest, amount: 10},
+        {legend: "Iphone X 16GB", sublegend:"8x de R$666,66", src:producttest, amount: 0},
+        {legend: "Iphone X 16GB", sublegend:"8x de R$666,66", src:producttest, amount: 0},
+        {legend: "Iphone X 16GB", sublegend:"8x de R$666,66", src:producttest, amount: 10},
+        {legend: "Iphone X 16GB", sublegend:"8x de R$666,66", src:producttest, amount: 0},
+        {legend: "Iphone X 16GB", sublegend:"8x de R$666,66", src:producttest, amount: 10}
+    ];
+        
+    const Test = products.map(product => (
+        <Product key={product.legend} legend={product.legend} sublegend={product.sublegend} src={product.src} amount={product.amount}/>
+    ));
+
+        
 	return(
 	    <MainDiv id="products">
 	      <Container>
-		<Row>
-		  {cols}
-		</Row>
+                <Row>
+                    {Test}
+                    {/* {cols} */}
+                </Row>
 	      </Container>
 	    </MainDiv>
 	);
@@ -101,3 +159,41 @@ class Products extends Component {
 }
 
 export default Products;
+
+
+
+
+
+
+// function retCols(){
+//     var ret = [];
+//     var number;
+//     if(imgSrcs.length == legends.length && legends.length === subLegends.length)
+// 	number = subLegends.length;
+//     else
+// 	number = 0;
+//     for(var i=0; i<number; i++)
+// 	ret.push(
+// 	    <Col xl="2" sm="4" xs="6">
+// 	      <ProdImg src={imgSrcs[i]}>
+//                 <Legend>{legends[i]}</Legend>
+//                 <SubLeg>{subLegends[i]}</SubLeg>
+// 	      </ProdImg>
+// 	    </Col>
+// 	);
+//     return ret;
+// }
+
+// const cols = retCols(6);
+
+
+// function createProducts(){
+//     var ret = [];
+//     for(var i=0; i<products.length; i++)
+// 	ret.push(
+// 	    <Product legend={products.legend} sublegend={products.sublegend} src={products.src}/>
+// 	);
+//     return ret;
+// }
+
+// const prod_col = createProducts();
